836e29c272110a299200d049558c1cc83a526bc8
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-08-04 00:05:01 +0800

    fix missing value

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ module.exports = function delegateProxy (target, origin) {
   return new Proxy(target, {
     get (target, key, receiver) {
       if (Reflect.has(target, key)) return Reflect.get(target, key, receiver)
-      const value = Reflect.get(origin, key, receiver)
+      const value = Reflect.get(origin, key)
       // Must bound origin
       if ('function' === typeof value) return value.bind(origin)
       return value
     },
-    set (target, key, receiver) {
-      return Reflect.has(target, key) ? Reflect.set(target, key, receiver) : Reflect.set(origin, key, receiver)
+    set (target, key, value, receiver) {
+      return Reflect.has(target, key) ? Reflect.set(target, key, value, receiver) : Reflect.set(origin, key, value)
     }
   })
 }
