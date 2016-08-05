c9854c97527d6b7dd83eb50db18dcf4b8b0540bb
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-08-05 12:27:09 +0800

    speedup

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 module.exports = function delegateProxy (target, origin) {
   return new Proxy(target, {
-    get (target, key) {
-      if (Reflect.has(target, key)) return target[key]
+    get (target, key, receiver) {
+      if (Reflect.has(target, key)) return Reflect.get(target, key, receiver)
       const value = origin[key]
       return 'function' === typeof value ? function method () {
         return value.apply(origin, arguments)
       } : value
     },
-    set (target, key, value) {
-      (Reflect.has(target, key) ? target : origin)[key] = value
+    set (target, key, value, receiver) {
+      if (Reflect.has(target, key)) return Reflect.set(target, key, value, receiver)
+      origin[key] = value
       return true
     }
   })
